b21ef0ddbcd73a071e99255cf838ef7e26d02dc2
Tashi Dolma <tadolma97@gmail.com>
2022-04-09 16:21:24 -0400

    commit tree 66929b431ec1868bb2a6590c0bd193fe54117449

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {};
+employee.name = sarah;
+employee.streetAddress = 45;
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    const newEmployee= {...employee};
+    newEmployee [name] = streetAddress;
+    return newEmployee
+}
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+     employee.name= 'Sam';
+     employee.streetAddress= '12 Broadway';
+     return employee
+ }
+ function deleteFromEmployeeByKey(employee, name, streetAddress){
+    const newEmployee= {...employee};
+    delete newEmployee.name;
+    return newEmployee
+ }
+function destructivelyDeleteFromEmployeeByKey(employee,name, streetAddress){
+    delete employee.name 
+    return employee
+}
